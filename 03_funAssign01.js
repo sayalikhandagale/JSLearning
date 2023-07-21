a887462f2d83e2166a58ecab00d824e1552b99b1
sayalikhandagale <sayalikhandagale7@gmail.com>
2023-07-21 14:48:47 +0530

    All The Changes are Committed and Pushed
    All The Changes are Committed and Pushed

---
+++ 03_funAssign01.js
@@ -0,0 +1,82 @@
+// Function with no argument and no return value
+console.log("---------------------------------------Step 1-------------------------------------------------------");
+function show(){
+    console.log('My Name is Sayli');
+    console.log('I Love Only JavaScript');
+}
+show();
+
+console.log("---------------------------------------Step 2--------------------------------------------------------");
+var firstName = "Sayli";
+var lastName = "Khandagale";
+var collegeName= "Kirti college";
+console.log("First Name:-", firstName, "Last Name:-", lastName,    "College Name:-", collegeName );
+
+console.log("---------------------------------------Step 3-------------------------------------------------------");
+var value1 = "Virat"; 
+var value2 = "Anushka";
+console.log( 'Before Swaping ==>' ,'value1 :- ', value1, 'value2 :- ', value2);
+var temp = value1;
+value1 = value2;
+value2 = temp;
+console.log( 'After Swaping ==>' ,'value1 :- ', value1, 'value2 :- ', value2);
+
+
+var num1 = 1000; 
+var num2 = 2000;
+console.log( 'Before Swaping==>' ,'num1 :- ', num1, ' num2 :- ', num2);
+var temp = num1;
+num1 = num2;
+num2 = temp;
+console.log( 'After Swaping==>' ,'num1 :- ', num1, ' num2 :- ', num2);
+
+console.log("---------------------------------------Step 4---------------------------------------------------------");
+function addition( arg1, arg2, arg3)
+{
+    var result=arg1+arg2+arg3;
+    console.log( 'Addition is :-', result);
+}
+addition(10.23,600,40)
+
+function addition( arg1, arg2, arg3)
+{
+    var result=arg1+arg2+arg3;
+    console.log( 'Addition is :-', result);
+}
+addition("Hello","Good","Morning")
+console.log("******************************************************************************************************");
+
+var bankName="CICI Bank";
+var accountnum=3456782345;
+var location="Pune";
+var pincode=431202 ;
+console.log("Bank Name:-", bankName, "Account Number:-", accountnum, "Location:-", location, "PinCode:-", pincode);
+
+var bankName="Axis Bank";
+var accountnum="7856782345";
+var location="Mumbai";
+var pincode=441202;
+console.log("Bank Name:-", bankName, "Account Number:-", accountnum, "Location:-", location, "PinCode:-", pincode);
+
+var bankName="HDFC Bank";
+var accountnum=8956782345;
+var location="Pune"
+var pincode=631202;
+console.log("Bank Name:-", bankName, "Account Number:-", accountnum, "Location:-", location, "PinCode:-", pincode);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
